3ea0a02b900e814f3bf2ca97d95c94e089967250
rlatldhs32 <rlatldhs33@naver.com>
2021-02-23 17:27:09 +0900

    upload files

---
+++ 백준풀기/소스.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		stack<char> a;
+		
+		string s;
+		s>>
+		a.push()
+	}
+}
